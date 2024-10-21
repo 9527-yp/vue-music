003e6001992e4f1ba9fbdd8ff6937b8ebc3d6048
15374373262 <www.1340983289@qq.com>
2024-10-22 00:09:54 +0800

    update:我的音乐歌单列表

--- src/views/my-music/MyMusic.vue
+++ src/views/my-music/MyMusic.vue
@@ -168,13 +168,108 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="song-list-box">
-                            <h3 class="title">歌曲列表</h3>
-                            <span class="song-num">134首歌</span>
-                            <div class="more">
-                                播放：<i class="num">4353535</i>次
-                            </div>
+                    </div>
+                    <div class="song-list-box">
+                        <h3 class="title">歌曲列表</h3>
+                        <span class="song-num">134首歌</span>
+                        <div class="more">
+                            播放：<i class="num">4353535</i>次
                         </div>
+                    </div>
+                    <div class="music-table">
+                        <table class="song-list-table">
+                            <thead>
+                                <tr>
+                                    <th class="first th-1">
+                                        <div class="text"></div>
+                                    </th>
+                                    <th>
+                                        <div class="text">歌曲标题</div>
+                                    </th>
+                                    <th class="th-2">
+                                        <div class="text">时长</div>
+                                    </th>
+                                    <th class="th-3">
+                                        <div class="text">歌手</div>
+                                    </th>
+                                    <th class="th-4">
+                                        <div class="text">专辑</div>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="music-list-c even">
+                                    <td class="tr-index">
+                                        <div class="index-hd">
+                                            <span class="index">1</span>
+                                            <i class="play-icn"></i>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="song-name">
+                                            <span class="text-hov">安河桥</span>
+                                        </div>
+                                    </td>
+                                    <td class="song-time-box">
+                                        <span class="song-time">04:56</span>
+                                        <div class="btns">
+                                            <i class="add-icn" title="添加到播放列表"></i>
+                                            <i class="icn collect-icn" title="收藏"></i>
+                                            <i class="icn share-icn" title="分享"></i>
+                                            <i class="icn down-icn" title="下载"></i>
+                                            <i class="icn del-icn" title="删除"></i>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="text">
+                                            <span class="text-hov">西语</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="text">
+                                            <span class="text-hov">安和桥</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="music-list-c">
+                                    <td class="tr-index">
+                                        <div class="index-hd">
+                                            <span class="index">1</span>
+                                            <i class="play-icn"></i>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="song-name">
+                                            <div class="song-name-box">
+                                                <span class="text-hov">安河桥</span>
+                                                <span class="song-other"> - (电影《爱情无限Touch》主题曲)</span>
+                                                <i class="mv-icn"></i>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="song-time-box">
+                                        <span class="song-time">04:56</span>
+                                        <div class="btns">
+                                            <i class="add-icn" title="添加到播放列表"></i>
+                                            <i class="icn collect-icn" title="收藏"></i>
+                                            <i class="icn share-icn" title="分享"></i>
+                                            <i class="icn down-icn" title="下载"></i>
+                                            <i class="icn del-icn" title="删除"></i>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="text">
+                                            <span class="text-hov">西语</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="text">
+                                            <span class="text-hov">安和桥</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -248,9 +343,9 @@ function login():void {
     overflow-y: scroll;
     position: relative;
     zoom: 1;
-    height: calc(100vh - 95px);
+    height: calc(100vh - 75px);
     .music-content{
-        min-height: calc(100vh - 95px);
+        min-height: calc(100vh - 75px);
         width: 980px;
         margin: 0 auto;
         border: 1px solid #d3d3d3;
@@ -657,31 +752,212 @@ function login():void {
                         }
                     }
                 }
-                .song-list-box{
+            }
+            .song-list-box{
+                width: 100%;
+                height: 35px;
+                border-bottom: 2px solid #c20c0c;
+                box-sizing: border-box;
+                padding: 0 10px 0 32px;
+                .title{
+                    font-size: 20px;
+                    line-height: 28px;
+                    float: left;
+                    font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
+                    font-weight: normal;
+                }
+                .song-num{
+                    margin: 9px 0 0 20px;
+                    float: left;
+                    color: #666;
+                }
+                .more{
+                    margin-top: 5px;
+                    float: right;
+                    color: #666;
+                    .num{
+                        color: #c20c0c;
+                    }
+                }
+            }
+            .music-table{
+                width: 100%;
+                height: 600px;
+                .song-list-table{
                     width: 100%;
-                    height: 35px;
-                    border-bottom: 2px solid #c20c0c;
-                    box-sizing: border-box;
-                    padding: 0 10px 0 32px;
-                    .title{
-                        font-size: 20px;
-                        line-height: 28px;
-                        float: left;
-                        font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
+                    border: 1px solid #d9d9d9;
+                    border-collapse: collapse;
+                    border-spacing: 0;
+                    table-layout: fixed;
+                    th{
+                        height: 38px;
+                        background-color: #f7f7f7;
+                        background: url('@/assets/images/my-music/table.png');
+                        background-position: 0 0;
+                        background-repeat: repeat-x;
+                        vertical-align: top;
+                        text-align: left;
                         font-weight: normal;
-                    }
-                    .song-num{
-                        margin: 9px 0 0 20px;
-                        float: left;
                         color: #666;
-                    }
-                    .more{
-                        margin-top: 5px;
-                        float: right;
-                        color: #666;
-                        .num{
-                            color: #c20c0c;
+                        .text{
+                            height: 18px;
+                            line-height: 18px;
+                            padding: 8px 10px;
+                            background: url('@/assets/images/my-music/table.png') no-repeat;
+                            background-position: 0 -56px;
                         }
+                    }
+                    td{
+                        padding: 6px 10px;
+                        line-height: 18px;
+                        text-align: left;
+                        background: url('@/assets/images/my-music/table.png') no-repeat 0 9999px;
+                    }
+                    .first{
+                        width: 74px;
+                        .text{
+                            background: none;
+                        }
+                    }
+                    .th-2{
+                        width: 111px;
+                    }
+                    .th-3{
+                        width: 14%;
+                    }
+                    .th-4{
+                        width: 20%;
+                    }
+                    .music-list-c{
+                        color: #333;
+                        .tr-index{
+                            .index-hd{
+                                height: 18px;
+                                .index{
+                                    margin-left: 5px;
+                                    color: #999;
+                                }
+                                .play-icn{
+                                    float: right;
+                                    width: 17px;
+                                    height: 17px;
+                                    cursor: pointer;
+                                    background: url('@/assets/images/my-music/table.png') no-repeat 0 9999px;
+                                    background-position: 0 -103px;
+                                }
+                            }
+                        }
+                        .song-name{
+                            height: 18px;
+                            margin-right: 20px;
+                            .song-name-box{
+                                position: relative;
+                                display: inline-block;
+                                padding-right: 25px;
+                                margin-right: -25px;
+                                max-width: 99%;
+                                height: 20px;
+                                overflow: hidden;
+                                text-overflow: ellipsis;
+                                white-space: nowrap;
+                                .song-other{
+                                    color: #aeaeae;
+                                }
+                                .mv-icn{
+                                    position: absolute;
+                                    top: 0;
+                                    right: 0;
+                                    width: 23px;
+                                    height: 17px;
+                                    margin: 1px 0 0 0;
+                                    background: url('@/assets/images/my-music/table.png') no-repeat 0 9999px;
+                                    background-position: 0 -151px;
+                                    cursor: pointer;
+                                    &:hover{
+                                        background-position: -30px -151px;
+                                    }
+                                }
+                            }
+                        }
+                        .song-time-box{
+                            .song-time{
+                                color: #666;
+                            }
+                            .btns{
+                                float: left;
+                                display: none;
+                                .add-icn{
+                                    margin-top: 2px;
+                                    float: left;
+                                    overflow: hidden;
+                                    vertical-align: middle;
+                                    width: 13px;
+                                    height: 13px;
+                                    background: url('@/assets/images/icon.png') no-repeat;
+                                    background-position: 0 -700px;
+                                    cursor: pointer;
+                                    &:hover{
+                                        background-position: -22px -700px;
+                                    }
+                                }
+                                .icn{
+                                    float: left;
+                                    width: 18px;
+                                    height: 16px;
+                                    margin: 2px 0 0 4px;
+                                    overflow: hidden;
+                                    text-indent: -999px;
+                                    background: url('@/assets/images/my-music/table.png') no-repeat;
+                                    cursor: pointer;
+                                }
+                                .collect-icn{
+                                    background-position: 0 -174px;
+                                    &:hover{
+                                        background-position: -20px -174px;
+                                    }
+                                }
+                                .share-icn{
+                                    background-position: 0 -195px;
+                                    &:hover{
+                                        background-position: -20px -195px;
+                                    }
+                                }
+                                .down-icn{
+                                    background-position: -81px -174px;
+                                    &:hover{
+                                        background-position: -104px -174px;
+                                    }
+                                }
+                                .del-icn{
+                                    background-position: 0 -217px;
+                                    &:hover{
+                                        background-position: -20px -217px;
+                                    }
+                                }
+                            }
+                        }
+                        .text{
+                            width: 100%;
+                            position: relative;
+                            zoom: 1;
+                            overflow: hidden;
+                            white-space: nowrap;
+                            text-overflow: ellipsis;
+                        }
+                        &:hover{
+                            .song-time-box{
+                                padding-right: 0px;
+                            }
+                            .song-time{
+                                display: none;
+                            }
+                            .btns{
+                                display: block;
+                            }
+                        }
+                    }
+                    .even{
+                        background-color: #f7f7f7;
                     }
                 }
             }
